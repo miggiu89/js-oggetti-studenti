69e918b3c1f8668e96aa9567e3adcff30ed40486
Matteo <matteomigliori280489@gmail.com>
2021-03-10 15:53:12 +0100

    terzo punto fatto, da sistemare

--- js/main.js
+++ js/main.js
@@ -8,9 +8,10 @@ var studente = {
     'età' : 24
 }
 //stampo in console con ciclo for
-for (var key in studente){
-    console.log(studente[key]);
-}
+
+//for (var key in studente){
+  //  console.log(studente[key]);
+//}
 
 /*Creare un array di oggetti di studenti. Ciclare su tutti gli
 studenti e stampare per ognuno nome e cognome*/
@@ -37,7 +38,20 @@ var studenti = [
 
 //stampo in console con ciclo for solo il nome e il cognome
 
-for (var key in studenti){
+/*for (var key in studenti){
     console.log(studenti[key]['nome']);
-    console.log(studenti[key]['cognome'])
+    console.log(studenti[key]['cognome']);
 }
+
+/*Dare la possibilità all’utente attraverso 3 prompt di
+aggiungere un nuovo oggetto studente inserendo
+nell’ordine: nome, cognome e età.*/
+
+var nuovoStudente = {
+    nome : prompt('inserisci il tuo nome'),
+    cognome : prompt('inserisci il cognome'),
+    età : prompt('inserisci l\'età'),
+};
+
+studenti.push(nuovoStudente);
+console.log(studenti);
